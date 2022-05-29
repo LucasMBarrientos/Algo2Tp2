e396d85391d96d2c779c1eb63c7db7f5690e0e30
Andres Guerrero <66138370+andrleo0@users.noreply.github.com>
2022-05-29 18:03:29 -0300

    primer comit de jugador

--- Jugador.cpp
+++ Jugador.cpp
@@ -1,34 +1,54 @@
 
 #include "Jugador.h"
-/**
- * pre:
- * pos:
- */
-Jugador::Jugador() {
+
+Jugador::Jugador(unsigned int id,std::string nombre){
+  if( id < 0 || id > MAX_JUGADORES){
+    throw "numero de valor invalido";
+  }
+  this->id = id;
+  this->nombre = nombre;
   this->cantidadDeSoldados = 0;
   this->cantidadDeDisparos = 0;
 }
 
-/**
- * pre:
- * pos:
- */
+unsigned int Jugador::obtenerId(){
+  return this->id;
+}
+
+std::string Jugador::obtenerNombre(){
+  return  this->nombre;
+}
+
+
+void Jugador::setcantidadDeSoldados(unsigned int cantidadDeSoldados){
+  this->cantidadDeSoldados = cantidadDeSoldados;
+}
+
+unsigned int Jugador::getcantidadDeSoldados(){
+  return this->cantidadDeSoldados;
+}
+
+void Jugador::setcantidadDeDisparos(unsigned int cantidadDeDisparos){
+  this->cantidadDeDisparos = cantidadDeDisparos;
+}
+
+unsigned int Jugador::getcantidadDeDisparos(){
+  return this->cantidadDeDisparos;
+}
+
 void Jugador::pedirCoordenadasDeAtaque() {
 
 }
 
-/**
- * pre:
- * pos:
- */
 void Jugador::pedirCoordenadasDeMovimiento() {
 
 }
 
-/**
- * pre:
- * pos:
- */
+void Jugador::eliminarSoldado(){
+
+}
+
 Jugador::~Jugador() {
+
 }
 
--- Jugador.h
+++ Jugador.h
@@ -3,24 +3,59 @@
 #define JUGADOR_H_
 
 #include <string>
-
+const unsigned int MAX_JUGADORES = 1000;
 
 class Jugador {
 
 	private:
-
-		std::string identificador; //  ID del jugador (puede ser otro tipo de dato)
+		unsigned int id;
+		std::string nombre; 
 		unsigned int cantidadDeSoldados;
 		unsigned int cantidadDeDisparos;
 
 	public:
+		/**
+		 * pre:
+		 * pos:
+		 */
+		Jugador(unsigned int id,std::string nombre);
 
 		/**
 		 * pre:
 		 * pos:
 		 */
-		Jugador();
+        unsigned int Jugador::obtenerId();
+		
+		/**
+		 * pre:
+		 * pos:
+		 */
+		std::string Jugador::obtenerNombre();
 
+		/**
+		 * pre:
+		 * pos:
+		 */
+		void Jugador::setcantidadDeSoldados(unsigned int cantidadDeSoldados);
+
+		/**
+		 * pre:
+		 * pos:
+		 */
+		unsigned int Jugador::getcantidadDeSoldados();
+
+		/**
+		 * pre:
+		 * pos:
+		 */
+		void Jugador::setcantidadDeDisparos(unsigned int cantidadDeDisparos);
+
+		/**
+		 * pre:
+		 * pos:
+		 */
+		unsigned int Jugador::getcantidadDeDisparos();
+		
 		/**
 		 * pre:
 		 * pos:
