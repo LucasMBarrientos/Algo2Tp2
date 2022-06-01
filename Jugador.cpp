f11c612fdb4b4c9aefaafa7ac3b913aefc26c95c
Ignak <83609489+IgnakG34@users.noreply.github.com>
2022-06-01 15:04:24 -0300

    Update Jugador.cpp

--- Jugador.cpp
+++ Jugador.cpp
@@ -1,33 +1,62 @@
 
 #include "Jugador.h"
-/**
- * pre:
- * pos:
- */
-Jugador::Jugador() {
 
+Jugador::Jugador(unsigned int id,std::string nombre,
+unsigned int cantidadDeSoldados,unsigned int cantidadDeDisparos){
+  if( id < 0 || id > MAX_JUGADORES){
+    throw "numero de valor invalido";
+  }
+  this->id = id;
+  this->nombre = nombre;
+  this->cantidadDeSoldados = cantidadDeSoldados;
+  this->cantidadDeDisparos = cantidadDeDisparos;
 }
 
-/**
- * pre:
- * pos:
- */
-void Jugador::pedirCoordenadasDeAtaque() {
-
+unsigned int Jugador::obtenerId(){
+  return this->id;
 }
 
-/**
- * pre:
- * pos:
- */
+std::string Jugador::obtenerNombre(){
+  return  this->nombre;
+}
+
+void Jugador::sumarUnSoldadoSoldados(){
+  this->cantidadDeSoldados++;
+}
+
+void Jugador::eliminarUnSoldados(){
+    this->cantidadDeSoldados--;
+}
+
+void Jugador::aumentarCantidadDisparos(unsigned int cantidadDeDisparos){
+  this->cantidadDeDisparos+= cantidadDeDisparos;
+}
+
+void Jugador::reducirCantidadDisparos(int cantidadDeDisparos){
+    if(cantidadDeDisparos > 0){
+      throw "numero invalido positivo";
+    }
+    this->cantidadDeDisparos-= cantidadDeDisparos;
+}
+
+void Jugador::pedirCoordenadasDeAtaque(unsigned int*x , unsigned int*y , unsigned int*z) {
+	std::cout<< "Ingrese una coordenada en X: ";
+	std::cin >> &x;
+	std::cout<< "Ingrese una coordenada en Y: ";
+	std::cin >> &y;
+	std::cout<< "Ingrese una coordenada en Z: ";
+	std::cin >> &z;
+}
+
 void Jugador::pedirCoordenadasDeMovimiento() {
 
 }
 
-/**
- * pre:
- * pos:
- */
+void Jugador::eliminarSoldado(){
+
+}
+
 Jugador::~Jugador() {
+
 }
 
